db21ed1135f9ab57f8a63143f48beedb102862bc
Daniel Miguel <117693873+DanielMigue@users.noreply.github.com>
2023-11-03 15:23:02 -0300

    Delete Avancando_SQL/Parte_03_LIKE.sql

--- Avancando_SQL/Parte_03_LIKE.sql
+++
@@ -1,9 +0,0 @@
-/*Usando Like*/
-
-SELECT * FROM tabela_de_produtos WHERE SABOR LIKE '%Maça%'; /*Ele traz todo mundo que tem "Maça"*/
-
-/*Se eue quiser consultar a palavra como se ela estivesse em último na frase devo colocar '%Maça'*/
-
-SELECT * FROM tabela_de_produtos WHERE SABOR LIKE '%Maça%'
-AND EMBALAGEM = 'PET'; /*Ele traz todo mundo que tem "Maça e PET"*/
-
