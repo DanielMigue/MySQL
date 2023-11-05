7082727c02ac23577eabc46de1ee4eea12ae19cf
Daniel Miguel <117693873+DanielMigue@users.noreply.github.com>
2023-11-05 12:18:27 -0300

    Consultas SQL: avançando no SQL com MySQL
    Manipulando com ORDER BY, ASC E DESC.

--- Avancando_SQL/Parte_06_OrderBy.sql
+++ Avancando_SQL/Parte_06_OrderBy.sql
@@ -4,7 +4,6 @@
 -ASC: Ascendente ou DESC: Descrescente
 
 */
-
 SELECT * FROM tabela_de_produtos;
 
 SELECT * FROM tabela_de_produtos ORDER BY PRECO_DE_LISTA;/*Ordena do menor para o maior o preço*/
@@ -21,5 +20,21 @@ serão exibidos de forma alfabética até o final. Ao término o produto da Emba
 
 SELECT * FROM tabela_de_produtos ORDER BY EMBALAGEM DESC, NOME_DO_PRODUTO ASC;/*Embalagem será do maior para o menor e produto será do menor para o maior*/
 
+/*
+EXERCÍCIO - MAIOR VENDA:
+
+Qual (ou quais) foi (foram) a(s) maior(es) venda(s) do produto “Linha Refrescante - 1 Litro - Morango/Limão”, 
+em quantidade? (Obtenha este resultado usando 2 SQLs).
+
+*/
+SELECT * FROM tabela_de_produtos WHERE NOME_DO_PRODUTO = 'Linha Refrescante - 1 Litro - Morango/Limão';/*Fiz a busca na tabela de produtos*/
+
+SELECT * FROM itens_notas_fiscais WHERE CODIGO_DO_PRODUTO = '1101035'/*Esse código está na chave primária da tabela_de_produtos, portanto consigo
+localiza-lo na tablea "itens_notas_fiscais" */
+
+ORDER BY QUANTIDADE DESC/* Dentro da tabela itens_notas_fiscais se encontra a coluna "quantidade" e aqui eu 
+faço o que o exercício perde que é buscar a maior venda através do "DESC" sendo ordenado pelo ORDER BY. */
+
+LIMIT 0,10;/*Aqui eu quis limitar por dez linhas pra ver se funcionava e sim funcionou*/
 
 
